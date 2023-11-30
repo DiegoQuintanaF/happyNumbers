7c8633f28f0516569f52222c856c9511245d2d54
Diego Quintana <dquintana@utb.edu.co>
2023-11-29 21:18:17 -0500

    update

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"math"
-	"time"
 
 	"github.com/diegoquinfa/happyNumbers/hn"
 )
@@ -12,8 +11,7 @@ func main() {
 	var a int64 = 1
 	var b int64 = int64(math.Pow(10, 16))
 	test := hn.NewHappyNumber()
-	start := time.Now()
 	result := test.CountHappyNumbers(a, b)
 
-	fmt.Printf("hay %d entre %d a %d y tardo %v\n", result, a, b, time.Since(start))
+	fmt.Printf("%d\n", result)
 }
